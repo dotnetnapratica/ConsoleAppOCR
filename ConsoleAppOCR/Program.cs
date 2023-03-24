da655ef171c6c32779c70f0a6cb8e31e3dff7b68
tiagomiranda.dev <tiagomiranda.dev@gmail.com>
2023-03-24 17:44:18 -0300

    initial commit

---
+++ ConsoleAppOCR/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tesseract;
+//Link para download tesseract windows 32 e 64 bits https://github.com/UB-Mannheim/tesseract/wiki
+Console.WriteLine("Ler texto da imagem");
+
+
+using (var engine = new TesseractEngine("C:\\Program Files\\Tesseract-OCR\\tessdata", "eng"))
+using(var img = Pix.LoadFromFile("input.jpg"))
+using(var page = engine.Process(img))
+{
+    var text = page.GetText();
+    Console.WriteLine($"Resultado: {text}");
+}
